81183722431653b672aa848af3fc401fae6db395
Võ Thanh Tùng <parkhoon1609@gmail.com>
2022-11-29 15:05:09 +0700

    change code

--- face_shot.py
+++ face_shot.py
@@ -1,11 +1,11 @@
 import cv2
 
-#Input your name
-name = 'Tung'
+name = 'Tung' #replace with your name
 
 cam = cv2.VideoCapture(0)
-cv2.namedWindow("Vui lòng nhấn space để chụp ảnh", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Vui lòng nhấn space để tiếp tục", 500, 300)
+
+cv2.namedWindow("press space to take a photo", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("press space to take a photo", 500, 300)
 
 img_counter = 0
 
@@ -14,14 +14,16 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("Nhấn space để chụp", frame)
+    cv2.imshow("press space to take a photo", frame)
 
-    k = cv2.waitkey(1)
-    if k % 256 == 27:
-        print("Nhấn Esc để kết thúc")
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing…")
         break
-    elif k % 256 == 32:
-        img_name = "dataset/" + name + "/image_{}.jpg".format(img_counter)
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
